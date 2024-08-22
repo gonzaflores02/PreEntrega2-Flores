53d0ab08bc807f31d38973a93423489c6918abb8
Gonzalo Flores <gonzalofranciscoflores@gmail.com>
2024-08-21 22:13:00 -0300

    modifique el main.js por completo

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-
-
-function sumaNumeros() {
-    let cantidadNumeros = parseInt(prompt("¿Cuantos números deseas sumar?"));
-    if(isNaN(cantidadNumeros)){
-        alert("Numero Invalido")
-        return;
-    }
-    let suma = 0
-    for (let i = 1; i <= cantidadNumeros; i++) {
-        let numero = parseInt(prompt("Ingresa el numero " + i + ":"));
-        if (isNaN(numero)) {
-            alert("¡Error! Ingresa un número valido.");
-            return;
-        } else {
-            suma = suma + numero
-        }
-    }
-    alert("La suma de los" + " " + cantidadNumeros + " " + "números ingresados es:" + " " + suma + ", Gracias!");
+function Producto(nombre, precio, stock, foto) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.foto = foto;
 }
 
-alert("Calculadora de sumas, en el caso de querer restar colocar el signo \" - \" en el numero correspondiente")
+let productos = [];
 
-sumaNumeros()
+while(true){
+    let pregunta = prompt("Quieres agregar un producto para la venta?, Si o No")
+    if (pregunta == "Si" || pregunta == "si"){
+        alert("Bien, vamos a agregar los productos");
+        let nombre = prompt("Ingresa el nombre del producto");
+        let precio = parseFloat(prompt("Ingrese el precio del producto:"));
+        let stock = parseFloat(prompt("Ingresa que cantidad del producto hay disponible"));
+        let foto = prompt("Ingresa el nombre de la imagen del producto");
+
+        const NuevoProducto = new Producto(nombre, precio, stock, foto)
+        productos.push(NuevoProducto)
+        // console.log(zapatilla);
+        alert(`El producto que agrego es ${NuevoProducto.nombre}, hay ${NuevoProducto.stock} unidades y su precio es ${NuevoProducto.precio}`)
+    }else if(pregunta == "No" || pregunta == "no"){
+        alert("Muchas gracias!");
+        break;
+    }else{
+        alert(`${pregunta} no es una respuesta válida. Por favor, responde "Si" o "No".`);
+    }
+}
+
+console.table(productos)
+
+for (const element of productos) {
+    console.log(element);
+}
+
+
